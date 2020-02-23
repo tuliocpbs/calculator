498447463800a56af46d35071f56ef9887b672e7
Tulio Silva <tuliocesarpbs@gmail.com>
2020-02-23 18:00:32 -0300

    Add logger and healthcheck utils

---
+++ consumer/src/utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+
+logger = logging.getLogger("consumer")
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
